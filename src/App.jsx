d3f49208ad12bc3bdc1da12dd9f8796e0e9c7cd2
Naga Sri Venkat Rentala <venkatrentala2001@gmail.com>
2024-09-28 17:24:44 +0530

    creating a form page and validate the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 
 import './App.css';
-import { UserProvider } from './Components/Usercontext';
-import UserForm from './Components/Userform';
-import UserList from './Components/Userlist';
+import Form from './Components/Form';
+// import { UserProvider } from './Components/Usercontext';
+// import UserForm from './Components/Userform';
+// import UserList from './Components/Userlist';
 
 function App() {
   return (
-    <UserProvider>
-      <div>
-        <h1>CRUD Operations with Use Context , Use Ref ,Use Reducer</h1>
-        <UserForm />
-        <UserList />
-      </div>
-    </UserProvider>
+    // <UserProvider>
+    //   <div>
+    //     <h1>CRUD Operations with Use Context , Use Ref ,Use Reducer</h1>
+    //     <UserForm />
+    //     <UserList />
+    //   </div>
+    // </UserProvider>
+    <div>
+      <Form />
+    </div>
   );
 }
 
---
+++ src/Components/Form.jsx
@@ -0,0 +1,277 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Form.css';
+import Tech from './image.png';
+import { FcGoogle } from 'react-icons/fc';
+import { FaFacebook} from 'react-icons/fa';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+function Form() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    let validateErrors = { ...errors };
+    if (!value) {
+      validateErrors[name] = `${
+        name.charAt(0).toUpperCase() + name.slice(1)
+      } is required `;
+    } else {
+      validateErrors[name] = "";
+    }
+    setErrors(validateErrors);
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  let phoneNumberPattern = /^([0-9]{10})$/
+  let emailPattern =
+    /^([a-zA-Z0-9]+)@([a-zA-Z0-9-]+).([a-zA-Z]+).([a-zA-Z]{2,20})$/;
+  let upperCasePattern = /[A-Z]/;
+  let lowerCasePattern = /[a-z]/;
+  let numberPattern = /[0-9]/;
+  let specialCharacterPattern = /[~!@#%&()$^_?]/;
+  let minlengthCharacterPattern = /^.{8,16}$/;
+  const validateForm = () => {
+    let valid = true;
+    let newErrors = [];
+    if (!formData.fullName) {
+      newErrors.fullName = "Full Name is required";
+      valid = false;
+    } else {
+      newErrors.fullName = " ";
+    }
+    if (!formData.phoneNumber) {
+      newErrors.phoneNumber = "Phone number is required";
+      valid=false;
+    }else{
+      if(!phoneNumberPattern.test(formData.phoneNumber)){
+        newErrors.phoneNumber = "Enter valid mobile number";
+        valid=false;
+      }else{
+        newErrors.phoneNumber = "";
+      }
+    }
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+      valid = false;
+    } else {
+      if (!emailPattern.test(formData.email)) {
+        newErrors.email = "Enter valid email";
+        valid = false;
+      } else {
+        newErrors.email = " ";
+      }
+    }
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+      valid = false;
+    } else {
+      if (!minlengthCharacterPattern.test(formData.password)) {
+        newErrors.password = "password must be between 8 to 16 characters";
+        valid = false;
+      } else if (!lowerCasePattern.test(formData.password)) {
+        newErrors.password = "At least 1 lowercase character";
+        valid = false;
+      } else if (!numberPattern.test(formData.password)) {
+        newErrors.password = "At least 1 number";
+        valid = false;
+      } else if (!specialCharacterPattern.test(formData.password)) {
+        newErrors.password = "At least 1 special character";
+        valid = false;
+      } else if (!upperCasePattern.test(formData.password)) {
+        newErrors.password = "At least 1 uppercase character";
+        valid = false;
+      } else {
+        newErrors.password = " ";
+      }
+    }
+    if (!formData.confirmPassword) {
+      newErrors.confirmPassword = "Confirm password is required";
+      valid = false;
+    } else if (formData.password !== formData.confirmPassword) {
+      newErrors.confirmPassword =
+        "Check your confirm password and password should be same";
+      valid = false;
+    } else {
+      newErrors.confirmPassword = "";
+    }
+    setErrors(newErrors);
+    return valid;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      console.log("Form submitted successfully")
+    }
+  }
+    const handleReset = () => {
+      setFormData({
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        userName: "",
+        password: "",
+        confirmPassword: "",
+      });
+      setErrors({
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        userName: "",
+        password: "",
+        confirmPassword: "",
+      });
+    };
+  return (
+    <div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xxl-10 col-lg-10 col-md d-md-flex parent">
+            <div className="col-md-8 text-center d-md-block d-none">
+              <div className="text-white tech">
+                <img className="text-center" src={Tech} alt="" />
+                <p className="text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque maiores <br />
+                  reprehenderit tempore eos molestias, cupiditate eius nobis
+                  voluptatem <br />
+                  veniam nesciunt expedita, deleniti blanditiis excepturi
+                  voluptas <br /> corrupti! Qui squam, sequi! Dolorum, maiores.
+                </p>
+                <div className="justify-content-between">
+                  <button className="btn btn-primary me-5">Sign In</button>
+                  <button className="btn btn-primary">Sign Up</button>
+                </div>
+                <div className="mt-4">
+                  <p>Or Sign Up With</p>
+                  <div>
+                    <button className="btn me-2">
+                      <FcGoogle className="fs-2" />
+                    </button>
+                    <button className="btn me-2">
+                      <FaFacebook className="text-primary fs-2" />
+                    </button>
+                    <button className="btn">
+                      <FaSquareXTwitter className="text-black fs-2" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 form p-4 my-md-4 mx-auto me-md-5 text-white">
+              <div className="">
+                <h3>Sign Up</h3>
+                <p className="para">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Deserunt velit itaque in .
+                </p>
+              </div>
+              <form action="" onSubmit={handleSubmit}>
+                <div className="">
+                  <label className="" htmlFor="">
+                    Name
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name='fullName'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={formData.fullName}
+                  />
+                  <span>{errors.fullName}</span>
+                </div>
+                <div className="">
+                  <label className="" htmlFor="">
+                    Phone Number
+                  </label>
+                  <input
+                    className="form-control"
+                    type="tel"
+                    name='phoneNumber'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={formData.phoneNumber}
+                  />
+                  <span>{errors.phoneNumber}</span>
+                </div>
+                <div className="">
+                  <label className="" htmlFor="">
+                    Email
+                  </label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    name='email'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={formData.email}
+                  />
+                  <span>{errors.email}</span>
+                </div>
+                <div className="">
+                  <label className="form-label" htmlFor="">
+                    Password
+                  </label>
+                  <input
+                    className="form-control"
+                    type="password"
+                    name='password'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={formData.password}
+                  />
+                  <span>{errors.password}</span>
+                </div>
+                <div className="">
+                  <label className="form-label" htmlFor="">
+                    Confirm Password
+                  </label>
+                  <input
+                    className="form-control"
+                    type="password"
+                    name='confirmPassword'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={formData.confirmPassword}
+                  />
+                  <span>{errors.confirmPassword}</span>
+                </div>
+                <div className="d-flex">
+                  <input className="form-check" type="checkbox" />
+                  <label className="form-label" htmlFor="">
+                    I agree terms of Service and Privacy Policy
+                  </label>
+                </div>
+                <div className="mt-3 text-center">
+                  <button
+                    className="btn btn-primary register px-4 py-2 me-3"
+                    type="submit"
+                  >
+                    Register
+                  </button>
+                  <button className='btn btn-primary register px-4 py-2' type='reset' onClick={handleReset}>Reset</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form
